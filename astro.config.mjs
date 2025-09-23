c97b1948eb524309f8841f3ea6698b461e5f2ab8
GeoSegun <seggyseggy97@gmail.com>
2025-09-23 22:22:37 +0100

    Configure Astro server to run on port 80 in astro.config.mjs.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    server: {
+        // * Use port 80 only in production, port 3000 for development
+        port: process.env.NODE_ENV === 'production' ? 80 : 3000
+    }
+});
