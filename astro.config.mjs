6d2b78894a1e97a003e1cd127364b052fb138a0f
GeoSegun <seggyseggy97@gmail.com>
2025-09-30 21:54:25 +0100

    Update preview:prod script in package.json to specify allowed hosts for production preview and remove Vite server configuration from astro.config.mjs.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,5 @@ export default defineConfig({
     server: {
         // * Use port 80 only in production, port 3000 for development
         port: process.env.NODE_ENV === 'production' ? 80 : 4321
-    },
-    vite: {
-        server: {
-            allowedHosts: ['web-t33n1d64wsm0.up-de-fra1-k8s-1.apps.run-on-seenode.com']
-        }
     }
 });
